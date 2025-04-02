41161d4880dd89b1e07d85d1fc051533732e39c2
IbaiOlea <iba_ole_goe@goierrieskola.org>
2025-04-02 08:49:45 +0200

    Mobileko konfigurazioa egiteko prest

--- Erronka3-Web/main.php
+++ Erronka3-Web/main.php
@@ -48,17 +48,21 @@ if (isset($_SESSION['user_id']) && $_SESSION['user_id'] != 0) {
 <header>
     <img src="M.S.N_Logo.png" alt="M.S.N_Logo">
 
-    <!-- Asegurar que el botón "Saioa Hasi" siempre aparezca -->
-    <a href="login.php" class="logout-button">Saioa hasi</a>
-
     <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] != 0): ?>
+        <!-- Usuario REGISTRADO (oculta "Saioa Hasi") -->
         <a href="produktuak.php" class="product-link">Zure produktuak</a>
         <a href="logout.php" class="logout-button">Saioa itxi</a>
         <a href="cesta.php" class="cart-button">Carrito (<?php echo array_sum($_SESSION['cesta']); ?>)</a>
 
     <?php elseif (isset($_SESSION['invitado']) && $_SESSION['invitado']): ?>
+        <!-- Modo INVITADO (muestra "Saioa Hasi" + carrito) -->
+        <a href="login.php" class="logout-button">Saioa hasi</a>
         <a href="productInvitedList.php" class="product-link">Produktu zerrenda</a>
         <a href="cesta.php" class="cart-button">Carrito (<?php echo array_sum($_SESSION['cesta']); ?>)</a>
+
+    <?php else: ?>
+        <!-- Sin sesión (solo "Saioa Hasi") -->
+        <a href="login.php" class="logout-button">Saioa hasi</a>
     <?php endif; ?>
 </header>
 

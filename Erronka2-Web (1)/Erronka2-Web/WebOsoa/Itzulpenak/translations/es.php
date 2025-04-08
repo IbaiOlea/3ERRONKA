7ba9f5e6998ddc7bae514f780820973446c57978
IbaiOlea <iba_ole_goe@goierrieskola.org>
2025-04-08 10:32:33 +0200

    Artxiboak kenduta

--- Erronka2-Web (1)/Erronka2-Web/WebOsoa/Itzulpenak/translations/es.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    "hello" => "Hola",
-    "goodbye" => "Adios",
-    "ourInfo" => "Nuestra información es la siguiente: Soy Ander de Legorreta",
-];
--- Erronka2-Web (1)/Erronka2-Web/WebOsoa/Itzulpenak/translations/eus.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    "hello" => "Kaixo", 
-    "goodbye" => "Agur",
-    "ourInfo" => "Gure informazioa ondorengoa da: Ander naiz Legorretakoa",
-];
